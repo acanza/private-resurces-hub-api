6f88177bc3244877d8b8a5cb09be6733cac9d830
Antonio Cantero <antonio@theclassyoga.com>
2026-06-08 11:31:57 +0200

    Add CORS configuration and allow origins based on environments

--- src/config.py
+++ src/config.py
@@ -7,6 +7,9 @@ class Settings(BaseSettings):
     ENVIRONMENT: str = "local"
     APP_TITLE: str = "Private Resources Hub API"
     APP_VERSION: str = "0.1.0"
+    ALLOWED_ORIGINS: dict[str, list[str]] = {
+        "local": ["http://localhost:3000"],  # desarrollo
+    }
 
 
 settings = Settings()
